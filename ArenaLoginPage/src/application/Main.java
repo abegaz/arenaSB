d1db50c929fad797b92f18ca27bc238a59884332
Jamie Arrowood-Forrester <31321068+jwarro3494@users.noreply.github.com>
2017-11-01 15:24:57 -0400

    Basic signup page
    ArenaSignUpPage

--- ArenaLoginPage/src/application/Main.java
+++ ArenaLoginPage/src/application/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -12,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Arena.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("SignUp.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
