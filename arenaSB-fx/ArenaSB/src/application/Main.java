12f54f507c8d6c88e971fed0b5a9e3fe8b984b29
callium <conner610@gmail.com>
2017-11-01 12:45:59 -0400

    Switched to webview, added index.html

--- arenaSB-fx/ArenaSB/src/application/Main.java
+++ arenaSB-fx/ArenaSB/src/application/Main.java
@@ -1,25 +1,30 @@
 package application;
 	
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
-
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../com/application/view/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		WebView browser = new WebView();
+		WebEngine engine = browser.getEngine();
+		
+		URL index = getClass().getResource("../com/application/view/index.html");
+		engine.load(index.toString());
+		
+		VBox root = new VBox();
+		root.getChildren().addAll(browser);
+		
+		Scene scene = new Scene(root, 600, 800);
+		primaryStage.setScene(scene);
+		
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
--- arenaSB-fx/ArenaSB/src/com/application/controller/MainController.java
+++ arenaSB-fx/ArenaSB/src/com/application/controller/MainController.java
@@ -1,5 +1,10 @@
 package com.application.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+
 public class MainController {
+	@FXML WebView webView;
+	
 	
 }
