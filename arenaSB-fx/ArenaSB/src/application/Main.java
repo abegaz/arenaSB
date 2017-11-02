a478a9c77e28e75048a51ee67bf6b58d8329b319
callium <conner610@gmail.com>
2017-11-02 16:17:21 -0400

    Added the JSObject listener and bridge class that contains functions we will need to submit forms

--- arenaSB-fx/ArenaSB/src/application/Main.java
+++ arenaSB-fx/ArenaSB/src/application/Main.java
@@ -3,13 +3,22 @@ package application;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.stage.Stage;
+import netscape.javascript.JSObject;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.concurrent.Worker.State;
 
 public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		WebView browser = new WebView();
@@ -25,9 +34,26 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		
 		primaryStage.show();
+		
+		
+		// engine.executeScript(script) // <--- Execute javascript from JavaFX code
+		
+		// Creates a JSObject, and can execute functions in the JavaFX Code
+		engine.getLoadWorker().stateProperty().addListener(
+				new ChangeListener<State>() {
+		            public void changed(ObservableValue ov, State oldState, State newState) {
+		                if (newState == State.SUCCEEDED) {
+			                	JSObject js = (JSObject) engine.executeScript("window"); 
+			            		js.setMember("app", new Bridge());
+		                }
+		            }
+		        });
+		
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public class Bridge {
+		public void submitUser() {
+			System.out.println("User form has been submitted");
+		}
 	}
 }
