ed6798392c4effe77e3a38a428a8a65f65b7aeee
Jamie Arrowood-Forrester <31321068+jwarro3494@users.noreply.github.com>
2017-11-04 12:34:33 -0400

    Update to controller

--- ArenaLoginPage/src/application/SignUpController.java
+++ ArenaLoginPage/src/application/SignUpController.java
@@ -1,5 +1,24 @@
 package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class SignUpController {
+
+     // Creates the objects.
+	@FXML private TextField emailTextField;
+	@FXML private TextField usernameTextField;
+	@FXML private PasswordField passwordPasswordField;
+	@FXML private Button detailedSignUpButton;
+	@FXML private Hyperlink detailedLogInHyperlink;
+	
+	// Initialize the objects. 
+	@FXML
+	 private void initialize() {
+		// Im not sure what is needed here at this time.
+		
+	}	
 	
 }
