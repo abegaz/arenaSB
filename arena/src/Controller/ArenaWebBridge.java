ab3205e1e2d28a796d0371552aa05245b5f8340c
Luke Brady <lbrad23105@gmail.com>
2017-11-09 23:08:30 -0500

    Added submitUser method.

--- arena/src/Controller/ArenaWebBridge.java
+++ arena/src/Controller/ArenaWebBridge.java
@@ -7,8 +7,8 @@ import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_256;;
 public class ArenaWebBridge {
 	public ArenaWebBridge() {}
 	
-	public void getCredentials(String userName, String passWord) {
-		String hashPassword = new DigestUtils(SHA_256).digestAsHex(passWord);
-		System.out.println("Username: " + userName + "\nPassword: " + hashPassword);
+	public void submitUser(String username, String password) {
+		String hashPassword = new DigestUtils(SHA_256).digestAsHex(password);
+		System.out.println("Username: " + username + "\nPassword: " + hashPassword);
 	}
 }
