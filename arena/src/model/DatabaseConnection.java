1b8e1635c85d4bf5428698ba322f21b6d099fb3a
Luke Brady <lbrad23105@gmail.com>
2017-11-09 22:54:34 -0500

    Added DatabaseConnection class.

--- arena/src/model/DatabaseConnection.java
+++ arena/src/model/DatabaseConnection.java
@@ -7,25 +7,33 @@ import java.sql.Statement;
 
 public class DatabaseConnection {
 	private static String DB_DRIVER = "com.mysql.jdbc.Driver";
-	private static String DB_URL = "67.205.191.64";
+	public String db_url;
+	public String db_user;
+	public String db_pass;
+	public Connection conn;
 	
-	private static String DB_USER = "root";
-	private static String DB_PASS = "arenasb";
+	DatabaseConnection(String db_url, String db_user, String db_pass) {
+		this.db_url = db_url;
+		this.db_user = db_user;
+		this.db_pass = db_pass;
+	}
 	
-	public static void newConnection() {
+	public void newConnection() {
 		Connection conn = null;
-		Statement stmt = null;
 		try {
 			Class.forName(DB_DRIVER);
-			
-			conn = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
-			
-		}catch(SQLException se) {
+			conn = DriverManager.getConnection(db_url,db_user,db_pass);
+		} catch(SQLException se) {
 		      //Handle errors for JDBC
 		      se.printStackTrace();
 		} catch(Exception e) {
 		      //Handle errors for Class.forName
 		      e.printStackTrace();
 		}
+		this.conn = conn;
+	}
+	
+	public void createUser(String email, String username, String password) {
+	
 	}
 }
