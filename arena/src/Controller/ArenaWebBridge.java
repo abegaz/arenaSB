150d14f50fe322353d9bc9f7f8433b987a3dbf51
Luke Brady <lbrad23105@gmail.com>
2017-11-09 22:27:18 -0500

    Added ArenaWebBridge.java.

---
+++ arena/src/Controller/ArenaWebBridge.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_256;;
+
+public class ArenaWebBridge {
+	public ArenaWebBridge() {}
+	
+	public void getCredentials(String userName, String passWord) {
+		String hashPassword = new DigestUtils(SHA_256).digestAsHex(passWord);
+		System.out.println("Username: " + userName + "\nPassword: " + hashPassword);
+	}
+}
