fc4ae20c45566f6d04cd730f527c951d487cdb96
Luke Brady <lbrad23105@gmail.com>
2017-11-13 21:36:47 -0500

    DO it`

--- arena/src/Controller/ArenaWebBridge.java
+++ arena/src/Controller/ArenaWebBridge.java
@@ -7,8 +7,11 @@ import model.DatabaseConnection;
 import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_256;;
 
 public class ArenaWebBridge {
-	public ArenaWebBridge() {}
-	DatabaseConnection db = new DatabaseConnection("jdbc:mysql://67.205.191.64/arena","root", "arenasb");
+	DatabaseConnection db;
+	public ArenaWebBridge() {
+		db = new DatabaseConnection("jdbc:mysql://67.205.191.64/arena","root", "arenasb");
+	}
+
 	
 	public void createUser(String username, String email, String password) {
 		String hashPassword = new DigestUtils(SHA_256).digestAsHex(password);
--- arena/src/model/DatabaseConnection.java
+++ arena/src/model/DatabaseConnection.java
@@ -4,7 +4,9 @@ import static org.apache.commons.codec.digest.MessageDigestAlgorithms.SHA_256;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
@@ -21,7 +23,6 @@ public class DatabaseConnection {
 		this.db_url = db_url;
 		this.db_user = db_user;
 		this.db_pass = db_pass;
-		Connection conn = null;
 		try {
 			Class.forName(DB_DRIVER);
 			conn = DriverManager.getConnection(db_url,db_user,db_pass);
@@ -32,35 +33,39 @@ public class DatabaseConnection {
 		      //Handle errors for Class.forName
 		      e.printStackTrace();
 		}
-		this.conn = conn;
 	}
 	
-	public void createUser(String username, String email, String password) {
-		String hashPassword = new DigestUtils(SHA_256).digestAsHex(password);
-		String query = "BEGIN;" + 
-				"INSERT INTO user (Username, Email, DateJoined) VALUES('"+ username +"', '"+email+"', NOW());" + 
-				"INSERT INTO passwords (UIDno, encrypted) VALUES(LAST_INSERT_ID(), '"+hashPassword+"');" + 
-				"COMMIT;";
+	public void createUser(String username, String email, String hashPassword) {
+		String queryUser = "INSERT INTO user (Username, Email, DateJoined) VALUES('"+ username +"', '"+email+"', NOW());"; 
+		String queryPassword = "INSERT INTO passwords (UIDno, encrypted) VALUES(LAST_INSERT_ID(), '"+hashPassword+"');";
 		try {
-			PreparedStatement prepStatement = (PreparedStatement) conn.prepareStatement(query);
-
+			conn.setAutoCommit(false);
+			PreparedStatement prepStatementUser = (PreparedStatement) conn.prepareStatement(queryUser);
+			PreparedStatement prepStatementPass = (PreparedStatement) conn.prepareStatement(queryPassword);
 		    // execute the preparedstatement
-		    prepStatement.execute();
-		    conn.setAutoCommit(false);
+		    prepStatementUser.execute();
+		    prepStatementPass.execute();
+		    conn.commit();
 		    conn.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Username: " + username + "\nPassword: " + password);
+		System.out.println("Username: " + username + "\nPassword: " + hashPassword);
 	}
 	
-	public void loginUser(String username, String password) {
+	public int loginUser(String username, String password) {
+		String storedPassword;
 		String hashPassword = new DigestUtils(SHA_256).digestAsHex(password);
 		String query = "SELECT encrypted FROM passwords as p," + 
 					   "user as u WHERE u.Username LIKE 'nameEntry' AND p.UIDno = u.UID";
 		try {
-			conn.prepareStatement(query);
+			Statement st = conn.createStatement();
+			ResultSet loginStatement = st.executeQuery(query);
+			while(loginStatement.next()) {
+				storedPassword = loginStatement.getString("p");
+			}
+			st.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
