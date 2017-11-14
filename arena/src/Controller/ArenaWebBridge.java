72c2072730a147b8b752274b4f6594cc218f3879
Luke Brady <lbrad23105@gmail.com>
2017-11-13 21:59:16 -0500

    Changed database connection type.

--- arena/src/Controller/ArenaWebBridge.java
+++ arena/src/Controller/ArenaWebBridge.java
@@ -10,10 +10,10 @@ public class ArenaWebBridge{
 
 	DatabaseConnection db;
 	public ArenaWebBridge() {
-		db = new DatabaseConnection("jdbc:mysql://67.205.191.64/arena","root", "arenasb");
 	}
 
 	public void createUser(String username, String email, String password) {
+		db = new DatabaseConnection("jdbc:mysql://67.205.191.64/arena","root", "arenasb");
 		String hashPassword = new DigestUtils(SHA_256).digestAsHex(password);
 		db.createUser(username, email, hashPassword);
 	}
